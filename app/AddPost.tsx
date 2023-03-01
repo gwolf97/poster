063237c5b8f0e2912667b7258afa452e3e7098bd
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2023-03-01 17:56:32 -0500

    Add post api

--- app/AddPost.tsx
+++ app/AddPost.tsx
@@ -1,22 +1,35 @@
 "use client"
 
-import { useState } from "react"
+import React, { useState } from "react"
+import {useMutation, useQueryClient} from "@tanstack/react-query"
+import axios from "axios"
 
 export default function CreatePsot(){
     const [title, setTitle] = useState("")
-    const [isDisabled, setDisable] = useState(false)
+    const [isDisabled, setDisabled] = useState(false)
+
+
+    //Create a post
+    const {mutate} = useMutation(
+        async (title: String) => await axios.post("/api/posts/addPost", {title})
+    )
+
+    const submitPost = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setDisabled(true)
+        mutate(title)
+    }
+
     return( 
-        <form className="bg-white my-8 p-8 rounded-md">
+        <form onSubmit={submitPost} className="bg-white my-8 p-8 rounded-md">
             <div className="flex flex-col my-4">
                 <textarea 
                     onChange={(e) => setTitle(e.target.value)} 
-                    name="title"
                     value={title}
+                    name="title"
                     placeholder="What`s on your mind?"
                     className="p-4 text-lg rounded-md my-2 bg-gray-200"
-                >
-                    {title}
-                </textarea>
+                />
             </div>
             <div className="flex items-center justify-between gap-2">
                 <p className={`font-bold text-sm ${title.length > 300 ? "text-red-700" : "text-gray700"}`}>{`${title.length}/300`}</p>
